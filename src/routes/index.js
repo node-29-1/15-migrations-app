da6827b9b4de141de85a03001003c6c4d59793b3
Andres Mendoza <andres.david.mm@hotmail.com>
2023-11-29 22:51:26 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const productRouter = require('./product.router');
+const movieRouter = require('./movie.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('/products', productRouter);
+router.use('/movies', movieRouter);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 const app = require('./app');
-const {exec} = require('child_process');
-
 
 const PORT = process.env.PORT || 8080;
 
@@ -8,17 +6,6 @@ const PORT = process.env.PORT || 8080;
 const main = async() => {
     try{
         app.listen(PORT);
-        await new Promise((resolve, reject) => {
-            const migrate = exec(
-              'sequelize db:migrate',
-              {env: process.env},
-              err => (err ? reject(err): resolve())
-            );
-          
-            // Forward stdout+stderr to this process
-            migrate.stdout.pipe(process.stdout);
-            migrate.stderr.pipe(process.stderr);
-          });
         console.log(`Server running on port ${PORT}`);
     } catch(error){
         console.log(error);
